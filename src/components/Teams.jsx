10347ad99b1132ee606a4e62f5b05991d4e33886
RubiyaHud <rubiya.hud@gmail.com>
2025-05-09 12:28:22 +0600

    Implement Slick Slider

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -5,25 +5,37 @@ import teamMember1 from '../assets/TeamMember1.png'
 import teamMember2 from '../assets/TeamMember2.png'
 import teamMember3 from '../assets/TeamMember3.png'
 import Image from './Image'
-import Flex from './Flex'
-
+import "slick-carousel/slick/slick.css";
+import Slider from "react-slick";
 
 
 const Teams = () => {
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    };
     return (
         <div className={"mb-[140px]"}>
             <Container>
                 <HeadingWithPara className={""} classH={"text-center pb-[15px]"} classP={"text-center m-auto"} hText={"Our Expert Team Member"} pText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
-                <Flex className={"mt-[50px] gap-[30px]"}>
-                    <Image className={""} imgSrc={teamMember1} imgAlt={"Team Member Image1"} />
-                    <Image className={""} imgSrc={teamMember2} imgAlt={"Team Member Image2"} />
-                    <Image className={""} imgSrc={teamMember3} imgAlt={"Team Member Image3"} />                    
 
-                </Flex>
+                <Slider {...settings}>
+                    <div>
+                        <Image className={""} imgSrc={teamMember1} imgAlt={"Team Member Image1"} />
+                    </div>
+                    <div>
+                        <Image className={""} imgSrc={teamMember2} imgAlt={"Team Member Image2"} />
+                    </div>
+                    <div>
+                        <Image className={""} imgSrc={teamMember3} imgAlt={"Team Member Image3"} />
+                    </div>
+                </Slider>
 
             </Container>
         </div>
     )
 }
-
 export default Teams
